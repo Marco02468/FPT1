7a3299b414f63a73fd76f791d80cb13f06ee47b7
Marco02468 <loesche1@gmx.de>
2017-05-08 13:53:12 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,39 @@
+package sample;
+
+import javax.swing.*;
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void handleButtonAction(ActionEvent event) {
+        System.out.print("click");
+    }
+
+    public void onBtnPlLoe(ActionEvent event) {
+        System.out.print("click");
+    }
+    public void onBtnPlLad(ActionEvent event) {
+        System.out.print("click");
+    }
+    public void onBtnOrdW(ActionEvent event) {
+        System.out.print("click");
+    }
+    public void onBtnShin(ActionEvent event) {
+        System.out.print("click");
+    }
+    public void onBtnSloe(ActionEvent event) {
+        System.out.print("click");
+    }
+
+    public void onBtnSnaechs(ActionEvent actionEvent) {
+    }
+
+    public void onBtnSletzt(ActionEvent actionEvent) {
+    }
+
+    public void onBtnSstart(ActionEvent actionEvent) {
+    }
+
+    public void onBtnMetaSp(ActionEvent actionEvent) {
+    }
+}
